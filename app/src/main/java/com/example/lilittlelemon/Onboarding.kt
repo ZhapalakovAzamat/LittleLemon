6d32ceae2d85e1ab3df74028f80a581466e6abd1
unknown <zhapalakov@gmail.com>
2025-05-10 21:04:14 +0500

    Onboarding

---
+++ app/src/main/java/com/example/lilittlelemon/Onboarding.kt
@@ -0,0 +1,160 @@
+package com.example.lilittlelemon
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Onboarding() {
+    val firstName = remember { mutableStateOf("") }
+    val lastName = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.logo
+            ),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(400.dp, 100.dp)
+                .fillMaxSize()
+                .padding(20.dp)
+        )
+        Box(
+            modifier = Modifier
+                .background(Color(0xFF485E57))
+                .size(400.dp, 100.dp)
+        ) {
+            Text(
+                text = "Let's get to know you",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 30.dp)
+            )
+        }
+        Box(
+            modifier = Modifier
+                .size(400.dp, 50.dp)
+        ) {
+            Text(
+                text = ("Personal information"),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 10.dp, start = 10.dp)
+            )
+        }
+        Text(
+            text = ("First name"),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp, start = 10.dp)
+        )
+        TextField(
+            value = firstName.value,
+            textStyle = TextStyle(fontSize = 25.sp),
+            shape = RoundedCornerShape(10.dp),
+            onValueChange = { newText -> firstName.value = newText },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+        Text(
+            text = ("Last name"),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp, start = 10.dp)
+        )
+        TextField(
+            value = lastName.value,
+            textStyle = TextStyle(fontSize = 25.sp),
+            shape = RoundedCornerShape(10.dp),
+            onValueChange = { newText -> lastName.value = newText },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+        Text(
+            text = ("Email"),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp, start = 10.dp)
+        )
+        TextField(
+            value = email.value,
+            textStyle = TextStyle(fontSize = 25.sp),
+            shape = RoundedCornerShape(10.dp),
+            onValueChange = { newText -> email.value = newText },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        )
+
+        Button(
+            onClick = { /*TODO*/ },
+            border = BorderStroke(1.dp, Color.Red),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(Color(0xFFDCAB3B)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "Register",
+                color = Color.Black
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun OnboardingPrewiew(){
+    Onboarding()
+}
